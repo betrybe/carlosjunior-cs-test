89f0ffb758b7099a7839d7b2937806348f09414b
cacajr <realcarlos7@hotmail.com>
2021-09-29 11:34:58 -0300

    adicionando requisito 2, mas com falha no teste mesmo retornando a string exata

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,10 +2,58 @@ from simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
-    
+    @classmethod
+    def quantity_product_per_employ(self, list_dict):
+        out = "Produtos estocados por empresa: \n"
+
+        if len(list_dict) == 0:
+            return out + '- Lista de produtos vazia'
+
+        if len(list_dict) == 1:
+            return out + "- " + list_dict[0]['nome_da_empresa'] + ": " + "1"
+
+        '''
+            guardaremos todas as empresas e posteriormente guardaremos
+            todas as empresas, sem repetições, para contarmos quantas vezes
+            elas se repetem na lista de produtos
+        '''
+        companies_list = []
+        for each_obj in list_dict:
+            companies_list.append(each_obj['nome_da_empresa'])
+
+        companies_list_unique = []
+        quantity_product = []
+
+        for each_company in companies_list:
+            if each_company not in companies_list_unique:
+                companies_list_unique.append(each_company)
+
+        for each_company in companies_list_unique:
+            freq = companies_list.count(each_company)
+            quantity_product.append(str(freq))
+
+        for i in range(len(companies_list_unique)):
+            out += "- "+companies_list_unique[i]+": "+quantity_product[i]+"\n"
+
+        return out
+
+    @classmethod
+    def generate(self, list_dict):
+        '''
+            quantidade de produtos estocados por empresa
+        '''
+        out = (
+            super().generate(list_dict) + "\n" +
+            self.quantity_product_per_employ(list_dict)
+        )
+
+        # print(out)
+
+        return out
 
 
-list = [
+'''
+products = [
     {
         "id": 1,
         "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP",
@@ -36,7 +84,7 @@ list = [
     {
         "id": 4,
         "nome_do_produto": "Uricum acidum, Benzoicum acidum",
-        "nome_da_empresa": "Newton Laboratories, Inc.",
+        "nome_da_empresa": "Newton Laboratories",
         "data_de_fabricacao": "2019-11-08",
         "data_de_validade": "2019-11-25",
         "numero_de_serie": "FR38 9203 3060 400T QQ8B HHS0 Q46",
@@ -44,4 +92,5 @@ list = [
     },
 ]
 
-CompleteReport.generate(list)
+CompleteReport.generate(products)
+'''
